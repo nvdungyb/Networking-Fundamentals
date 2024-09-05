dadc6ff9ad36166944a27841a4c13d169c4431b8
NvDzung <dung3032003md@gmail.com>
2024-09-05 22:49:18 +0700

    Using lambda expression for setMethod

--- simplehttpserver/src/main/java/com/nvdungyb/httpserver/http/HttpRequest.java
+++ simplehttpserver/src/main/java/com/nvdungyb/httpserver/http/HttpRequest.java
@@ -5,7 +5,8 @@ import java.util.Arrays;
 public class HttpRequest extends HttpMessage {
     private HttpMethod method;
     private String requestTarget;
-    private String httpVersion;
+    private String originalHttpVersion;         // literal from request.
+    private HttpVersion bestCompatibleHttpVersion;
 
     HttpRequest() {
     }
@@ -21,5 +22,27 @@ public class HttpRequest extends HttpMessage {
                 .orElseThrow(() -> new HttpParsingException(HttpStatusCode.SERVER_ERROR_501_NOT_IMPLEMENTED));
 
         this.method = method;
+    }
+
+    public void setRequestTarget(String target) throws HttpParsingException {
+        if (target == null || target.length() == 0)
+            throw new HttpParsingException(HttpStatusCode.SERVER_ERROR_500_INTERNAL_SERVER_ERROR);
+
+        this.requestTarget = target;
+    }
+
+    public String getRequestTarget() {
+        return this.requestTarget;
+    }
+
+    void setHttpVersion(String originalHttpVersion) throws BadHttpVersionException, HttpParsingException {
+        this.originalHttpVersion = originalHttpVersion;
+        this.bestCompatibleHttpVersion = HttpVersion.getBestCompatibleVersion(originalHttpVersion);
+        if (this.bestCompatibleHttpVersion == null)
+            throw new HttpParsingException(HttpStatusCode.SERVER_ERROR_505_HTTP_VERSION_NOT_SUPPORTED);
+    }
+
+    public HttpVersion getBestCompatibleHttpVersion() {
+        return this.bestCompatibleHttpVersion;
     }
 }
