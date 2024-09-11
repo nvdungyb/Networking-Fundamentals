42de54c1823cae193af0f1a56b6fc504c537aa5c
NvDzung <dung3032003md@gmail.com>
2024-09-11 15:59:16 +0700

    Update driver class

--- simplehttpserver/src/main/java/com/nvdungyb/httpserver/HttpServer.java
+++ simplehttpserver/src/main/java/com/nvdungyb/httpserver/HttpServer.java
@@ -1,20 +1,38 @@
 package com.nvdungyb.httpserver;
 
-import com.nvdungyb.httpserver.config.Configuration;
 import com.nvdungyb.httpserver.config.ConfigurationManager;
+import com.nvdungyb.httpserver.config.HttpConfiguration;
+import com.nvdungyb.httpserver.config.HttpConfigurationAndResources;
+import com.nvdungyb.httpserver.config.TargetResources;
+import com.nvdungyb.httpserver.core.ServerListenerThread;
 
-import java.io.IOException;
+import java.util.HashMap;
+import java.util.logging.Logger;
 
 /**
  * Driver class for Http server.
  */
 public class HttpServer {
-    public static void main(String[] args) throws IOException {
-        System.out.println("Server starting...");
+    private final static Logger logger = Logger.getLogger(HttpServer.class.getName());
 
-        ConfigurationManager.getInstance().loadConfigurationFile("simplehttpserver/src/main/resources/http.json");
-        Configuration conf = ConfigurationManager.getInstance().getCurrentConfiguration();
-        System.out.println(conf.getPort());
-        System.out.println(conf.getWebroot());
+    public static void main(String[] args) {
+        logger.info("Server starting...");
+
+        HttpConfiguration httpConfiguration = ConfigurationManager.getInstance().loadConfigurationFile("simplehttpserver/src/main/resources/http.json", HttpConfiguration.class);
+        HashMap<String, String> resources = ConfigurationManager.getInstance().loadConfigurationFile("simplehttpserver/src/main/resources/request_target.json", HashMap.class);
+        TargetResources targetResources = new TargetResources(resources);
+
+        HttpConfigurationAndResources httpConfigurationAndResources = HttpConfigurationAndResources.getInstance();
+        httpConfigurationAndResources.setHttpConfiguration(httpConfiguration);
+        httpConfigurationAndResources.setTargetResources(targetResources);
+
+        System.out.println(httpConfigurationAndResources);
+
+        try {
+            ServerListenerThread serverListenerThread = new ServerListenerThread(httpConfiguration.getPort(), httpConfiguration.getWebroot());
+            serverListenerThread.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
