36f6437156d76134bf78023eff92469fba94686d
NvDzung <dung3032003md@gmail.com>
2024-09-11 15:46:49 +0700

    Parsing the header and body of the http request

--- simplehttpserver/src/main/java/com/nvdungyb/httpserver/http/HttpParser.java
+++ simplehttpserver/src/main/java/com/nvdungyb/httpserver/http/HttpParser.java
@@ -1,9 +1,9 @@
 package com.nvdungyb.httpserver.http;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
 import java.util.logging.Logger;
 
 public class HttpParser {
@@ -14,20 +14,18 @@ public class HttpParser {
     private static final int LF = 0x0A; // 10
 
     public HttpRequest parseHttpRequest(InputStream inputStream) throws HttpParsingException {
-        InputStreamReader reader = new InputStreamReader(inputStream, StandardCharsets.US_ASCII);
-
         HttpRequest request = new HttpRequest();
         try {
-            parseRequestLine(reader, request);
+            parseRequestLine(inputStream, request);
+            parseHeaders(inputStream, request);
+            parseBody(inputStream, request);
         } catch (IOException e) {
         }
-        parseHeaders(reader, request);
-        parseBody(reader, request);
 
         return request;
     }
 
-    private void parseRequestLine(InputStreamReader reader, HttpRequest request) throws IOException, HttpParsingException {
+    private void parseRequestLine(InputStream reader, HttpRequest request) throws IOException, HttpParsingException {
         StringBuilder processingDataBuffer = new StringBuilder();
 
         boolean methodParsed = false;
@@ -78,9 +76,93 @@ public class HttpParser {
         }
     }
 
-    private void parseHeaders(InputStreamReader reader, HttpRequest request) {
+    private void parseHeaders(InputStream inputStream, HttpRequest request) throws IOException {
+        HashMap<String, String> header = new HashMap<>();
+        int _byte;
+        StringBuilder sb = new StringBuilder();
+        while (inputStream.available() > 0 && (_byte = inputStream.read()) >= 0) {
+            if (_byte == CR) {
+                _byte = inputStream.read();
+                if (_byte == LF) {
+                    if (sb.length() > 0) {
+                        String data = sb.toString();
+                        sb.delete(0, sb.length());
+                        int indexOfColon = data.indexOf(':');
+                        try {
+                            String headContent = data.substring(0, indexOfColon);
+                            String content = data.substring(indexOfColon + 2);          // remove ':' and SP.
+                            header.put(headContent, content);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    } else {
+                        break;
+                    }
+                }
+            } else {
+                sb.append((char) _byte);
+            }
+        }
+
+        for (String key : header.keySet()) {
+            System.out.println(key + " : " + header.get(key));
+        }
+        request.setHeader(header);
     }
 
-    private void parseBody(InputStreamReader reader, HttpRequest request) {
+    private void parseBody(InputStream inputStream, HttpRequest request) throws IOException {
+        String boundary = null;
+        boolean isBoundary = true;
+        boolean isData = false;
+        int headerBytes = 0;
+        int bytesData = 0;
+        int contentLength = 0;
+        try {
+            contentLength = Integer.parseInt(request.getHeader().get("Content-Length"));
+        } catch (Exception e) {
+            logger.info("Can not read Content-Length in header!");
+        }
+
+        StringBuilder sb = new StringBuilder();
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        int _byte;
+        while (inputStream.available() > 0 && (_byte = inputStream.read()) >= 0) {
+            if (!isData) {
+                sb.append((char) _byte);
+                if (_byte == CR) {
+                    _byte = inputStream.read();
+                    sb.append((char) _byte);
+                    if (_byte == LF) {
+                        if (sb.length() > 2) {
+                            if (isBoundary) {
+                                boundary = sb.toString();
+                                isBoundary = false;
+                            } else {
+                                String content = sb.toString();
+                                if (content.contains("filename=")) {
+                                    int indexOfFilename = content.indexOf("filename=");
+                                    String filename = content.substring(indexOfFilename + 10, content.length() - 3);
+                                    request.setFilename(filename);
+                                }
+                            }
+                            headerBytes += sb.length();
+                            sb.delete(0, sb.length());
+                        } else {
+                            headerBytes += 2;
+                            bytesData = contentLength - (boundary.getBytes().length + 2 + 2 + headerBytes);             // More details : Notes/how_to_do
+                            isData = true;
+                        }
+                    }
+                }
+            } else {
+                if (bytesData > 0) {
+                    outputStream.write(_byte);
+                    bytesData--;
+                }
+            }
+        }
+
+        if (outputStream.size() > 0)
+            request.setData(outputStream.toByteArray());
     }
 }
